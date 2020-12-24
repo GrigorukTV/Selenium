07cfb1be56ac5ba202c868af580220ff3f457dad
Mikhail <dadykinm@yandex.ru>
2020-12-24 20:59:07 +0200

    Работа с протоколами ssh и ftp с помощью библиотеки Paramiko

--- lesson12/index.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from tests.TestHomePage import TestHomePage
-from tests.TestCatalog import TestCatalog
-from tests.TestCatalogId import TestCatalogId
-from tests.TestAccountLogin import TestAccountLogin
-from tests.TestAccountLoginAdm import TestAccountLoginAdm
-from tests.TestAdmin import TestAdmin
